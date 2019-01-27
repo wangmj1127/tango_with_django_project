990aa836d4bc745a647189d0f35d9b016bf6f70f
Mengjia Wang <2349520w@student.gla.ac.uk>
2019-01-27 02:01:43 +0000

    update chapter 6 exercise

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,8 @@ from rango.models import Page
 def index(request):
     #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 
     return render(request, 'rango/index.html', context=context_dict)
 
